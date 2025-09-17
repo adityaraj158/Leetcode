7e7428b2e0af73fb3c69751b31c027f0bc27bb43
Aditya Raj Choudhary <157699922+adityaraj158@users.noreply.github.com>
2025-09-17 21:59:29 +0530

    Time: 0 ms (100%), Space: 8.9 MB (35.85%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     bool checkIfPangram(string sentence) {
         vector<int> arr(26,0);
-        for(char i:sentence){
-            int j=i-'a';
+        
+        for(int i=0;i<sentence.length();i++){
+            int j = sentence[i] - 'a';
             arr[j]++;
         }
-        for(int &count:arr){
-            if(count==0){
+        
+        for(int j=0;j<arr.size();j++){
+            if(arr[j] == 0){
                 return false;
             }
         }
